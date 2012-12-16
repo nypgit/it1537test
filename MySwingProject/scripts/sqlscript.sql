99ab2342a773a99b3e56cebd733be61af0daaeae
Kheng Kok <khengkok@gmail.com>
2012-12-16 23:39:34 +0800

    added SQL script

---
+++ MySwingProject/scripts/sqlscript.sql
@@ -0,0 +1,24 @@
+--<ScriptOptions statementTerminator=";"/>
+
+ALTER TABLE memberids DROP PRIMARY KEY;
+
+ALTER TABLE member DROP PRIMARY KEY;
+
+DROP TABLE memberids;
+
+DROP TABLE member;
+
+CREATE TABLE memberids (
+	member_id INT NOT NULL,
+	PRIMARY KEY (member_id)
+) ENGINE=InnoDB;
+
+CREATE TABLE member (
+	member_id VARCHAR(30) NOT NULL,
+	name VARCHAR(45),
+	tel VARCHAR(12),
+	username VARCHAR(45) NOT NULL,
+	password VARCHAR(45) NOT NULL,
+	PRIMARY KEY (member_id)
+) ENGINE=InnoDB;
+
